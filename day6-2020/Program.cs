94322668104bb50839f4514b69760f9ced518336
Charl P. Botha <cpbotha@vxlabs.com>
2021-11-28 22:06:53 +0200

    Add day 6 2020 solution

---
+++ day6-2020/Program.cs
@@ -0,0 +1,6 @@
+using aoclib;
+
+// split on double \n to split on blank line between passports
+var groups = Utils.GetTrimmedInput(@"input.txt").Split("\n\n");
+
+Console.WriteLine("Hello, World!");
