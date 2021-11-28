6b63cfc575760f65869ad100cf21aa42e5730650
Charl P. Botha <cpbotha@vxlabs.com>
2021-11-28 15:58:26 +0200

    Add two formulations of part 2 solution

--- day5-2020/Program.cs
+++ day5-2020/Program.cs
@@ -1,25 +1,40 @@
 ﻿using aoclib;
 
-// split on double \n to split on blank line between passports
 var boardingPasses = Bleh.GetTrimmedInput(@"input.txt").Split("\n");
 
-Console.WriteLine(boardingPasses.Select(GetSeatId).Max());
+// we're going to need this again, so materialise
+var allIds = boardingPasses.Select(GetSeatId).ToHashSet();
+var maxId = allIds.Max();
 
-//Console.WriteLine(GetSeatId("FBFBBFFRLR"));
+Console.WriteLine($"Part 1 highest seat ID: {maxId}");
+
+// imperative form:
+int mySeatId = -1;
+for (var seatId = allIds.Min()+1; seatId < maxId; seatId++) {
+    if (!allIds.Contains(seatId) && allIds.Contains(seatId-1) && allIds.Contains(seatId+1)) {
+        mySeatId = seatId;
+        break;
+    }
+}
+
+// more functional:
+var mySeatId2 = Enumerable.Range(allIds.Min() + 1, allIds.Count - 1).First(seatId =>
+    !allIds.Contains(seatId) && allIds.Contains(seatId - 1) && allIds.Contains(seatId + 1));
+
+Console.WriteLine($"Part 2 my seat ID: {mySeatId2}");
 
 int GetSeatId(string seatCode)
 {
-    // surprisingly, I got this right at the second try
+    // surprisingly, I got this linq query right at the second try
     int rowNum = seatCode.Substring(0, 7)
         // only F represents 1 bit; shift left so that you would get 64 for the first one, and so on
-        .Select((rowChar, idx) => rowChar switch { 'B' => 1 << 6 - idx, _ => 0 })
+        .Select((rowChar, idx) => rowChar == 'B' ? 1 << 6 - idx : 0)
         // OR everything together
         .Aggregate((x, y) => x | y);
 
     int colNum = seatCode.Substring(7, 3)
-        .Select((rowChar, idx) => rowChar switch { 'R' => 1 << 2 - idx, _ => 0 })
+        .Select((rowChar, idx) => rowChar == 'R' ? 1 << 2 - idx : 0)
         .Aggregate((x,y) => x | y);
 
     return rowNum * 8 + colNum;
-
 }
