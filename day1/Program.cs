ef9d90eab2f96261c7fa7bed7260615e87ed848a
Charl P. Botha <cpbotha@vxlabs.com>
2021-12-01 21:37:55 +0200

    Add day 1 solutions

---
+++ day1/Program.cs
@@ -0,0 +1,36 @@
+using aoclib;
+
+var numbers = Utils.GetTrimmedInput(@"input.txt").Split("\n").Select(int.Parse).ToList();
+
+// part 1 submitted:
+int numDeeper = 0;
+for (var i = 1; i < numbers.Count(); i++) {
+    if (numbers[i] - numbers[i-1] > 0) numDeeper++;
+}
+
+Console.WriteLine(numDeeper);
+
+// part 2 submitted
+// in pandas I would use .rolling() -> .shift() -> subtract and count
+// f# has .windowed()
+// c# nothing built-in for this: https://stackoverflow.com/questions/8874901/is-there-an-equivalent-to-the-f-seq-windowed-in-c
+int numDeeper2 = 0;
+for (var i = 2; i < numbers.Count() - 1; i++) {
+    var cur = numbers[i-1] + numbers[i] + numbers[i+1];
+    var prev = numbers[i-2] + numbers[i-1] + numbers[i];
+    if (cur - prev > 0) numDeeper2++;
+}
+
+Console.WriteLine(numDeeper2);
+
+// BONUS round starts here =============================================================================================
+
+// part 1 after finding https://stackoverflow.com/a/3874078/532513
+Console.WriteLine($"p1 v2: {numbers.Zip(numbers.Skip(1), (current, next) => next - current).Count(e => e > 0)}");
+
+// zip three sequences to calculate the rolling sum
+// consider numbers.Select((num, idx) => use idx to sum shifted, taking care of overflows) and so on...
+var summedDepth = numbers.Zip(numbers.Skip(1), numbers.Skip(2)).Select((x) => x.First+x.Second+x.Third);
+// zip the rolling sum with a shifted version of itself to calculate the depth gradient
+var numDeeper2V2 = summedDepth.Zip(summedDepth.Skip(1), (current, next) => next - current).Count(e => e > 0);
+Console.WriteLine($"p2 v2: {numDeeper2V2}");
