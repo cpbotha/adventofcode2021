f2d7b06cc462a7c7776ff01ab2c9a3bcb52e52f1
Charl P. Botha <cpbotha@vxlabs.com>
2021-12-06 10:54:38 +0200

    Use dictionary to track fish

--- day6/Program.cs
+++ day6/Program.cs
@@ -1,13 +1,54 @@
 using aoclib;
-var fish = Utils.GetTrimmedInput(@"test_input.txt").Split(",").Select(int.Parse).ToList();
 
-for (var day = 1; day <= 256; day++)
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+
+var fish = Utils.GetTrimmedInput(@"input.txt").Split(",").Select(int.Parse).ToList();
+
+// the trick we employ here is to work on the histogram domain
+var fishDict = fish.GroupBy(x => x).ToDictionary(x => x.Key, x => (long)x.Count());
+
+// set to 80 for part 1, 256 for part 2
+var lastDay = 256;
+for (var day = 1; day <= lastDay; day++)
 {
-    fish = MakeFish(fish);
-    Console.WriteLine($"day {day} -- {fish.Count} fishies");// -- {string.Join(",", fish)}");
+    // naive part 1
+    //fish = MakeFish(fish);
+
+    var outDict = new Dictionary<int, long>();
+
+    // make some new fishies
+    if (fishDict.GetValueOrDefault(0) > 0)
+    {
+        // each of these fish will produce a new fish
+        outDict[8] = fishDict[0];
+        // all of the 0 fishies will now move to 6
+        outDict[6] = fishDict[0];
+    }
+
+    // age the rest
+    for (int fishNum = 1; fishNum <= 8; fishNum++)
+    {
+        if (fishDict.GetValueOrDefault(fishNum) > 0)
+        {
+            // move to fishNum - 1
+            //if (!outDict.ContainsKey(fishNum - 1)) outDict[fishNum - 1] = 0;
+            // TryAdd effectively does the same as the commented-out check and set above
+            // https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.dictionary-2.tryadd?view=net-5.0
+            outDict.TryAdd(fishNum - 1, 0);
+            outDict[fishNum - 1] += fishDict[fishNum];
+        }
+    }
+
+    fishDict = outDict;
+    
+    //Console.WriteLine($"day {day} -- {fishDict.Values.Sum()} fishies");// -- {string.Join(",", fish)}");
 }
 
+watch.Stop();
+Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
 
+Console.WriteLine($"day {lastDay} -- {fishDict.Values.Sum()} fishies");// -- {string.Join(",", fish)}");
 
 List<int> MakeFish(List<int> inputFish)
 {
@@ -27,6 +68,7 @@ List<int> MakeFish(List<int> inputFish)
 
     // ARGH why did this never manage to modify the closed newFish?
     // the TFish inside the select would change its version of the newFish var, but not the outside one
+    // could be https://stackoverflow.com/questions/11287070/c-sharp-select-query-not-modifying-external-variable
     //var outputFish = inputFish.Select(TFish);
 
     for (int i = 0; i < inputFish.Count; i++)
